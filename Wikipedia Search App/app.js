be2393613e1c907dfe7c7a574315a06a0a6da329
Nitin Mittal <nitin2000mittal@gmail.com>
2022-12-29 13:24:23 +0530

    completed the api call

--- Wikipedia Search App/app.js
+++ Wikipedia Search App/app.js
@@ -1,11 +1,52 @@
-const searchForm = document.querySelector("#searchForm");
-const searchInput = document.querySelector("#searchInput");
-const input = searchInput.target.value;
-const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${input}`;
+let searchForm = document.getElementById('searchForm');
 
-function displaySearchResult(txt){
-    console.log(txt);
+let searchInput = document.getElementById('searchInput');
+
+
+searchForm.addEventListener('submit', (e)=> {
+e.preventDefault();
+displaySearchResults(searchInput.value);
+})
+function displaySearchResults(x){
+    let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${x}`;
+
+    fetch(url)
+    .then(function(response) {
+     return(response.json());
+     })
+     .then(function(data){
+        let resultsArray = data.query.search;
+        resultsOnPage(resultsArray);
+     })
+     .catch(function() {
+     console.log('An error occurred');
+     });
+
 }
 
+function resultsOnPage(myArray){
+    resultsList.innerHTML = " ";
+    resultsList.insertAdjacentHTML('beforeend', `<h2>Search Results for ${searchInput.value} </h2>`);
 
-searchForm.addEventListener('submit',displaySearchResult(url));
+    myArray.forEach(function(item){
+    let itemTitle = item.title;
+    let itemSnippet = item.snippet;
+    let itemUrl = encodeURI(`https://en.wikipedia.org/wiki/${item.title}`);
+
+    resultsList.insertAdjacentHTML('beforeend',
+    `<div class="resultItem">
+    <h3 class="resultTitle">
+    <a href="${itemUrl}" target="_blank" rel="noopener">${itemTitle}</a>
+    </h3>
+    <p class="resultSnippet"><a href="${itemUrl}"  target="_blank" rel="noopener">
+    ${itemSnippet}</a></p>
+    </div>`
+);
+    
+
+  
+    
+    
+    })
+    
+};    
